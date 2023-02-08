1c7cc933b795e50d9d2447d02783ab47a451a666
Alexia Calmon <alexia.calmon@mercadolivre.com>
2023-02-08 17:48:53 -0300

    Parte 04

--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,16 +1,6 @@
 package com.generation.blogpessoal.model;
 
 import java.time.LocalDateTime;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.*;
