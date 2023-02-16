e1944dd26d65355023b8b1e3433768c7193a08f7
Alexia Calmon <alexia.calmon@mercadolivre.com>
2023-02-16 10:32:25 -0300

    Docker

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -1,6 +1,7 @@
 package com.generation.blogpessoal.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -20,6 +21,7 @@ public class Usuario {
     @NotNull(message = "O Atributo Nome é Obrigatório!")
     private String nome;
 
+    @Schema(example = "email@email.com.br")
     @NotNull(message = "O Atributo Usuário é Obrigatório!")
     @Email(message = "O Atributo Usuário deve ser um email válido!")
     private String usuario;
